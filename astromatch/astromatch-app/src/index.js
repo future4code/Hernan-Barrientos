037ddb4c659e84b7c4f588ef3e952f2303e63833
hernanbarrientos <hrn.2211@gmail.com>
2021-08-28 12:06:35 -0300

    atualizacao-exercicio-useEffect

--- astromatch/astromatch-app/src/index.js
+++ astromatch/astromatch-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  rootElement
+  document.getElementById('root')
 );
+
